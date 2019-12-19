d5cae8daa1eac3c0f08e96b60a04de708f5b1cd3
Mukesh Parmar <52579167+mukesh-98@users.noreply.github.com>
2019-12-20 01:48:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Baka Cafe
   It is a website which helps to order from the Baka Cafe. 
-warning
-  it is not responsive
-  
--- mail.php
+++ mail.php
@@ -32,7 +32,6 @@ if(isset($_POST['submit'])) {
 
 
 		$fname = $_POST['fname']; 
-	        $lname = $_POST['lname'];
 		$phone=$_POST['phone'];
 		$email=$_POST['email'];
 		$password=$_POST['password'];
